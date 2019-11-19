85f010a6768fbb6e4c335c79ad52c8e370668ba0
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2019-11-19 23:35:34 +0000

    Changed tests from Facts to data driven Theories.

--- RabbitMQDemo/Messaging.Tests/Connection/BasicConnectionFactoryTests.cs
+++ RabbitMQDemo/Messaging.Tests/Connection/BasicConnectionFactoryTests.cs
@@ -26,11 +26,13 @@ namespace Messaging.Tests.Connection
             Assert.Equal(password, basicConnectionFactory.Password);
         }
 
-        [Fact]
-        public void BasicConnectionFactory_WhenHostNameIsNull_ThenArgumentException()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void BasicConnectionFactory_WhenHostNameIsNotSupplied_ThenArgumentException(string hostname)
         {
             // Arrange
-            string hostname = null;
             string username = "testUser";
             string password = "password123";
 
@@ -48,12 +50,14 @@ namespace Messaging.Tests.Connection
             Assert.Throws<ArgumentException>(instantiation);
         }
 
-        [Fact]
-        public void BasicConnectionFactory_WhenHostNameIsEmpty_ThenArgumentException()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void BasicConnectionFactory_WhenUsernameIsNotSupplied_ThenArgumentException(string username)
         {
             // Arrange
-            string hostname = string.Empty;
-            string username = "testUser";
+            string hostname = "127.0.0.1";
             string password = "password123";
 
             // Act
@@ -70,145 +74,15 @@ namespace Messaging.Tests.Connection
             Assert.Throws<ArgumentException>(instantiation);
         }
 
-        [Fact]
-        public void BasicConnectionFactory_WhenHostNameIsWhiteSpace_ThenArgumentException()
-        {
-            // Arrange
-            string hostname = "  ";
-            string username = "testUser";
-            string password = "password123";
-
-            // Act
-            void instantiation()
-            {
-                BasicConnectionFactory basicConnectionFactory = new BasicConnectionFactory(
-                    hostname,
-                    username,
-                    password);
-
-            }
-
-            // Assert
-            Assert.Throws<ArgumentException>(instantiation);
-        }
-
-        [Fact]
-        public void BasicConnectionFactory_WhenUsernameIsNull_ThenArgumentException()
-        {
-            // Arrange
-            string hostname = "127.0.0.1";
-            string username = null;
-            string password = "password123";
-
-            // Act
-            void instantiation()
-            {
-                BasicConnectionFactory basicConnectionFactory = new BasicConnectionFactory(
-                    hostname,
-                    username,
-                    password);
-
-            }
-
-            // Assert
-            Assert.Throws<ArgumentException>(instantiation);
-        }
-
-        [Fact]
-        public void BasicConnectionFactory_WhenUsernameIsEmpty_ThenArgumentException()
-        {
-            // Arrange
-            string hostname = "127.0.0.1";
-            string username = string.Empty;
-            string password = "password123";
-
-            // Act
-            void instantiation()
-            {
-                BasicConnectionFactory basicConnectionFactory = new BasicConnectionFactory(
-                    hostname,
-                    username,
-                    password);
-
-            }
-
-            // Assert
-            Assert.Throws<ArgumentException>(instantiation);
-        }
-
-        [Fact]
-        public void BasicConnectionFactory_WhenUsernameIsWhiteSpace_ThenArgumentException()
-        {
-            // Arrange
-            string hostname = "127.0.0.1";
-            string username = "  ";
-            string password = "password123";
-
-            // Act
-            void instantiation()
-            {
-                BasicConnectionFactory basicConnectionFactory = new BasicConnectionFactory(
-                    hostname,
-                    username,
-                    password);
-
-            }
-
-            // Assert
-            Assert.Throws<ArgumentException>(instantiation);
-        }
-
-        [Fact]
-        public void BasicConnectionFactory_WhenPasswordIsNull_ThenArgumentException()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void BasicConnectionFactory_WhenPasswordIsNotSupplied_ThenArgumentException(string password)
         {
             // Arrange
             string hostname = "127.0.0.1";
             string username = "testUser";
-            string password = null;
-
-            // Act
-            void instantiation()
-            {
-                BasicConnectionFactory basicConnectionFactory = new BasicConnectionFactory(
-                    hostname,
-                    username,
-                    password);
-
-            }
-
-            // Assert
-            Assert.Throws<ArgumentException>(instantiation);
-        }
-
-        [Fact]
-        public void BasicConnectionFactory_WhenPasswordIsEmpty_ThenArgumentException()
-        {
-            // Arrange
-            string hostname = "127.0.0.1";
-            string username = "testUser";
-            string password = string.Empty;
-
-            // Act
-            void instantiation()
-            {
-                BasicConnectionFactory basicConnectionFactory = new BasicConnectionFactory(
-                    hostname,
-                    username,
-                    password);
-
-            }
-
-            // Assert
-            Assert.Throws<ArgumentException>(instantiation);
-        }
-
-        [Fact]
-        public void BasicConnectionFactory_WhenPasswordIsWhiteSpace_ThenArgumentException()
-        {
-            // Arrange
-            string hostname = "127.0.0.1";
-            string username = "testUser";
-            string password = "  ";
 
             // Act
             void instantiation()
