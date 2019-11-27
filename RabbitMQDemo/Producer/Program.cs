35b9426778b819dca2f7b574bcdb993bee0efb19
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2019-11-27 22:38:23 +0000

    Added simple console program to produce a message to RabbitMQ.

--- RabbitMQDemo/Producer/Program.cs
+++ RabbitMQDemo/Producer/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using Messaging;
+using Messaging.Connection;
+using Messaging.Contracts;
+using Messaging.Producing;
+using Messaging.Producing.Contracts;
+using RabbitMQ.Client;
+using Utilities;
+using Utilities.Contracts;
 
 namespace Producer
 {
@@ -6,7 +14,33 @@ namespace Producer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Dependencies.
+            IDateTimeGenerator dateTimeGenerator = new DateTimeGenerator();
+            ITypeConverter typeConverter = new TypeConverter();
+            ExternalMechanismFactory externalMechanismFactory = new ExternalMechanismFactory();
+
+            SecureConnectionSettings connectionSettings = new SecureConnectionSettings(
+                "raspberrypi",
+                "/path/to/client/certiicate.p12",
+                "password");
+
+            ExternalAuthConnectionFactory sslAuthConnectionFactory = new ExternalAuthConnectionFactory(
+                "raspberrypi",
+                externalMechanismFactory,
+                connectionSettings);
+
+            Settings producingSettings = new Settings("Journeys", "journey");
+
+            // Generate journey message.
+            Journey journey = new Journey(150, 35.5, 121.3, "Weather was wet and windy.");
+            IMessage<Journey> message = new Message<Journey>(dateTimeGenerator, journey);
+
+            // Produce the message.
+            using IMessageProducer messageProducer = new MessageProducer(sslAuthConnectionFactory, typeConverter);
+            messageProducer.Produce(producingSettings, message);
+
+            Console.WriteLine($"Message sent. ID: {message.Data.Id}");
+            Console.ReadLine();
         }
     }
 }
